d3e033ae1b2fe3338ffbe931b30a6c8386bc233c
wongyuhao <54234367+wongyuhao@users.noreply.github.com>
2019-11-27 20:37:21 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
+/*****************************
+* Recipe Book Program:
+* This program allows the users to enter details 
+* of recipes, including name, calories, all ingredients
+* and all steps to make the recipe.
+*
+* It can also load recipes from a txt file and save them
+* to a txt file as well.
+*
+* The goal of this program is to preserve the heritage of cooking
+* by storing traditional recipes easily, so that they can be passed 
+* on to future generations without being destroyed on paper.
+*/
+
+
+
 #include "Recipe.h"
 #include <iostream>
 using namespace std;
 
 int main() {
-	while (true) {
+	while (true) {  //while loop allows user to loop back to main menu during the program
+		
 		cout << "Recipe Book v1.0\n\n"
 			<< "[1] View Recipes\n"
 			<< "[2] Enter New Recipe\n"
@@ -14,29 +31,29 @@ int main() {
 		cout << "Please enter the number beside your selection:\n";
 
 		int selection;
-		cin >> selection;
+		cin >> selection; //read menu selection from user
 		cout << '\n';
 
-		validate(selection, 0, 4);
+		validate(selection, 0, 4); //validate if user input is within range for this menu
 
-		switch (selection) {
+		switch (selection) { //open a menu or a function based on the user's selection
 		case 1:
-			viewRecipes();
+			viewRecipes(); //calls function thath opens the view recipe menu
 			break;
 		case 2:
-			addRecipe();
+			addRecipe(); //calls function that prompts user for recipe details
 			break;
 		case 3:
-			save();
+			save(); //saves all current recipes to a .txt file in specific directory
 			break;
 		case 4:
-			load();
+			load(); //reads.txt file from a specific directory and copies recipes into the program
 			break;
 		case 0:
-			cout << "Exiting...\n";
+			cout << "Exiting...\n"; //exits the progam
 			return 0;
 		default:
-			cerr << "Unexpected error in recipe_main.cpp: main()\n";
+			cerr << "Unexpected error in recipe_main.cpp: main()\n"; //catches an error if there is unexpected input
 			return 0;
 		}
 	}
